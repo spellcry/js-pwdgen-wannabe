983dc328c846973b28f28a1030e4ef9c1d57bda3
spellcry <sangiothebest@hotmail.com>
2022-07-19 14:35:18 +0200

    soluzione

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// chiedo nome cognome e colore all'utente
+let nome = prompt("Qual è il tuo nome?");
+nome = nome.toLowerCase();
+let cognome = prompt("Qual è il tuo cognome?");
+cognome = cognome.toLowerCase();
+let colore = prompt("Qual è il tuo colore preferito?");
+colore = colore.toLowerCase();
+
+// genero un numero casuale tra 0 e 100
+const min = 0;
+const max = 100;
+const numRandom = Math.floor(Math.random() * (max - min) + min);
+
+console.log(nome+cognome+colore+numRandom);
+document.getElementById("result").innerHTML = `${nome+cognome+colore+numRandom}`;
