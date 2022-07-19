f5431167db38c5d631988ee1251068ffe1bb8807
spellcry <sangiothebest@hotmail.com>
2022-07-19 15:20:15 +0200

    modified random intervals

--- bonus/js/scripts.js
+++ bonus/js/scripts.js
@@ -13,9 +13,10 @@ const utente = {
         this.colore = colore.toLowerCase();
     },
     getPassword() {
-        // genero un numero casuale tra 0 e 100 (escluso)
-        const min = Math.ceil(0);
-        const max = Math.floor(100);
+        // genero un numero casuale
+        // chiedo l'intervallo numerico all'utente
+        const min = Math.ceil(parseInt(prompt("Inserisci un numero compreso tra 0 e 49")));
+        const max = Math.floor(parseInt(prompt("Inserisci un numero compreso tra 51 e 100")));
         const numRandom = Math.floor(Math.random() * (max - min) + min);
         return `${this.nome+this.cognome+this.colore+numRandom}`;
     }
