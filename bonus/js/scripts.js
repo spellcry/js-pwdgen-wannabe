7b5df7987b67900f138bdd7a95b7d56acafb346c
spellcry <sangiothebest@hotmail.com>
2022-07-20 12:14:08 +0200

    fix random intervals

--- bonus/js/scripts.js
+++ bonus/js/scripts.js
@@ -14,10 +14,10 @@ const utente = {
     },
     getPassword() {
         // genero un numero casuale
-        // chiedo l'intervallo numerico all'utente
+        // chiedo l'intervallo numerico all'utente (estremi inclusi)
         const min = Math.ceil(parseInt(prompt("Inserisci un numero compreso tra 0 e 49")));
         const max = Math.floor(parseInt(prompt("Inserisci un numero compreso tra 51 e 100")));
-        const numRandom = Math.floor(Math.random() * (max - min) + min);
+        const numRandom = Math.floor(Math.random() * (max - min + 1) + min);
         return `${this.nome+this.cognome+this.colore+numRandom}`;
     }
 }
